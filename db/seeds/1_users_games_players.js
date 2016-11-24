2a7128988455641ffd54e5fdf70c5649313d8cdb
Eric Fredine <eric.fredine@gmail.com>
2016-11-24 19:28:33 +0000

    Update seeding so it uses automatically incremented primary keys.

---
+++ db/seeds/1_users_games_players.js
@@ -0,0 +1,29 @@
+function insertPlayers(knex, Promise, userIds, gameIds) {
+  return Promise.all([
+    knex('players').insert({game_id: gameIds[0], user_id: userIds[0], won: true}),
+    knex('players').insert({game_id: gameIds[0], user_id: userIds[1], won: false}),
+    knex('players').insert({game_id: gameIds[1], user_id: userIds[0], won: true}),
+    knex('players').insert({game_id: gameIds[1], user_id: userIds[1], won: false}),
+    knex('players').insert({game_id: gameIds[2], user_id: userIds[0], won: true}),
+    knex('players').insert({game_id: gameIds[2], user_id: userIds[2], won: false})
+  ]);
+}
+
+exports.seed = function(knex, Promise) {
+  return Promise.all([
+    knex('players').del(), knex('games').del(), knex('users').del()])
+    .then(function () {
+      return knex('users').insert([{name: 'Alice'}, {name: 'Bob'}, {name: 'Charlie'}])
+        .returning('id')
+        .then(userIds => {
+          return knex('games').insert([
+              {created_at: new Date(), updated_at: new Date()},
+              {created_at: new Date(), updated_at: new Date()},
+              {created_at: new Date(), updated_at: new Date()}])
+          .returning('id')
+          .then(gameIds => {
+            return insertPlayers(knex, Promise, userIds, gameIds);
+          });
+        });
+    });
+};
