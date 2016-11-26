737ec29f60cbae799ea7fc4f92f719c952b4d289
Jamal Alawes <jamal.alawes@gmail.com>
2016-11-25 22:07:50 -0800

    appropriately creates new game board, including cards for p1, p2, and prize

--- public/scripts/createCards.js
+++ public/scripts/createCards.js
@@ -1,46 +1,100 @@
+//decides suit of cards for each PLAYER
+const suits = ["diams", "hearts", "spades", "clubs"];
+const cardNumbers = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"]
+
+function Shuffle(arrayOfCards) {
+	for(var j, x, i = arrayOfCards.length; i; j = parseInt(Math.random() * i), x = arrayOfCards[--i], arrayOfCards[i] = arrayOfCards[j], arrayOfCards[j] = x);
+	return arrayOfCards;
+};
+
+function newGame() {
+  const newSuits = Shuffle(suits);
+  const p1_suit = newSuits[0];
+  const p2_suit = newSuits[1];
+  const prize_suit = newSuits[2];
+
+  //create whole hand for p1
+  for (var p1Index = 1; p1Index < 14; p1Index += 1) {
+    generatePlayerHand(p1_suit, p1Index);
+  }
+
+  //create hand for p2
+  for (var p2Index = 1; p2Index < 14; p2Index += 1) {
+    createOpponentHand(p2Index);
+  }
+
+  for (var prizeIndex = 1; prizeIndex < 14; prizeIndex += 1) {
+    createRemainingPrizes(prizeIndex);
+  }
+  return;
+}
+
+
+
 
 // CREATES A NEW CARD TO ADD TO PLAYER HAND
-function generatePlayerCard(card) { //*****throwing in artifical information for testing
-  console.log("Creating new player card.");
-
+function generatePlayerHand(p1_suit, cardNumber) { //*****throwing in artifical information for testing
+  if (cardNumber === 1) {
+    cardNumber = "A";
+  }
+  else if (cardNumber === 11) {
+    cardNumber = "J"
+  }
+  else if (cardNumber === 12) {
+    cardNumber = "Q";
+  }
+  else if (cardNumber === 13) {
+    cardNumber = "K";
+  }
   const newCard = $([
-    //card must be placed (appended) into an already existing <ul>CARD GOES HERE</ul>
     "<li>",
-      "<a class='card rank-A diams' href='#'>", // TODO: need to add href TODO: REPLACE DIV CLASS
-        "<span class='rank'>A</span>",
-        "<span class='suit'>&diams;</span>",
+      `<a class='card rank-${cardNumber} ${p1_suit}' href='#'>`,
+        `<span class='rank'>${cardNumber}</span>`,
+        `<span class='suit'>&${p1_suit};</span>`,
       "</a>",
     "</li>"
   ].join("\n"));
   $( "#playerHand" ).append( newCard );
+  return;
 }
 
 
-
-
-// FUNCTION THAT SHOWS THE NUMBER OF CARDS REMAINING IN THE OPPONENT'S HAND
-function createOpponentHand(num) {
-  console.log("Attempting to add new card to opponents hand.");
-
-  for (var i = 0; i < num; i += 1) {
-
-    const newOppCard = $([
-      "<li><div class='card back'>*</div></li>"
-    ].join("\n"));
-
-    $( "#oppHand" ).append( newOppCard );
-  };
-
+//SHOWS CARDS REMAINING IN THE OPPONENT'S HAND
+function createOpponentHand(cardNumber) {
+  if (cardNumber === 1) {
+    cardNumber = "A";
+  }
+  if (cardNumber === 11) {
+    cardNumber = "J"
+  }
+  if (cardNumber === 12) {
+    cardNumber = "Q";
+  }
+  if (cardNumber === 13) {
+    cardNumber = "K";
+  }
+  const newOppCard = $(["<li><div class='card back'>*</div></li>"].join("\n"));
+  $( "#oppHand" ).append( newOppCard );
+  return;
 }
 
 
-function createRemainingPrizes(number) {
-  for (var prizeIndex = 0; prizeIndex < number; prizeIndex += 1) {
-
-    const prizeCard = $([
-      "<li><div class='card back'>*</div></li>"
-    ].join("\n"));
-
-    $( "#prizeCards" ).append( prizeCard );
-  };
+function createRemainingPrizes(cardNumber) {
+  if (cardNumber === 1) {
+    cardNumber = "A";
+  }
+  else if (cardNumber === 11) {
+    cardNumber = "J"
+  }
+  else if (cardNumber === 12) {
+    cardNumber = "Q";
+  }
+  else if (cardNumber === 13) {
+    cardNumber = "K";
+  }
+  const prizeCard = $([
+    "<li><div class='card back'>*</div></li>"
+  ].join("\n"));
+  $( "#prizeCards" ).append( prizeCard );
+  return;
 }
--- public/scripts/gameBoard.js
+++ public/scripts/gameBoard.js
@@ -2,9 +2,6 @@
 "use strict";
 
 function createEmptyBoard() {
-  console.log("Attempting to create an empty game board...");
-  //var that contains the gameboard html
-
   const emptyBoard = $([
 
     //WHOLE CONTAINER
@@ -26,15 +23,16 @@ function createEmptyBoard() {
           "<div class='playingCards'>",
 
 
-
-
             //OPPONENTS PLAYED CARD
             "<div class='opponentsPlayedCards center'>",
               "<div class='playingCards'>",
+
                 "<div class='card little joker'>",
                   "<span class='rank'>-</span>",
                   "<span class='suit'>Joker</span>",
                 "</div>",
+
+
               "</div>",
             "</div>",
             // /OPPONENTS PLAYED CARD
@@ -55,16 +53,9 @@ function createEmptyBoard() {
               //     "<span class='suit'></span>",
               // "</div>",
 
-              // PLAYERS PLAYED CARD
-              "<ul>",
-                "<li>",
-                  "<a class='card rank-2 diams' href='#'>", //artifical card ****** REMOVE
-                    "<span class='rank'>2</span>",
-                    "<span class='suit'>&diams;</span>",
-                  "</a>",
-                "</li>",
-              "</ul>",
-              // /PLAYERS PLAYED CARD
+              // PLAYER PLAYED CARD
+              "<ul id='playerPlayedCards'></ul>",
+              // /PLAYER PLAYED CARD
 
 
 
@@ -78,7 +69,7 @@ function createEmptyBoard() {
       //BOTTOM
       "<div class='container container-bottom'>",
         "<div class='playingCards'>",
-          "<ul class='hand' id='playerHand'><li><a class='card rank-7 diams' href='#'><span class='rank'>7</span><span class='suit'>&diams;</span></a></li></ul>", // ARTIFICAL CARD ******* REMOVE
+          "<ul class='hand' id='playerHand'></ul>",
           "<div class='clear'></div>", //clear
         "</div>",
       "</div>",
