b2a1d0125c5307e790982cd6c558e0b674040445
Kelly James <kellyjamesclancy@gmail.com>
2016-11-26 00:42:45 +0000

    new-game.js in progress

---
+++ public/scripts/new-game.js
@@ -0,0 +1,35 @@
+$(() => {
+
+  // function gameContainer(gameBoard) {
+  //   // $("#game-container").append(createGameBoard(game));
+  //   $("#game-container").append($("<h1>").text("GAME BOARD"));
+  //
+  // }
+
+  // function createGameBoard(game) {
+  //   $("<h1>").text("GAME BOARD");
+  // }
+
+  $(".new-game").on("click", (event) => {
+    event.preventDefault();
+    
+    $.ajax({
+      method: 'POST',
+      url: '/api/games/new'
+    })
+    .then((game) => {
+        // gameContainer(gameData);
+        console.log(game);
+      $("#game-container").append($(
+        `<h1>${game.game_id}</h1>
+        <ul>
+          ${game.users.map((user) => `<li>${user.username}</li>`)}
+        </ul>
+        `
+      ))
+    })
+    .fail(err => console.error(`/api/games/new: ${err}`))
+
+  });
+
+});
