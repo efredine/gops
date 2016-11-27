5879e63609a26b6c486629aafa0696fda022ab61
Eric Fredine <eric.fredine@gmail.com>
2016-11-27 00:22:51 +0000

    Removed dead socket.io reference from server

--- server.js
+++ server.js
@@ -4,7 +4,6 @@ require('dotenv').config();
 
 const PORT          = process.env.PORT || 8080;
 const ENV           = process.env.ENV || "development";
-// const socketio      = require('socket.io');
 const express       = require("express");
 const bodyParser    = require("body-parser");
 const sass          = require("node-sass-middleware");
